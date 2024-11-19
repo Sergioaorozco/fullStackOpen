e0199dc37b11c0d61ca3dde182abaf5d4edc1670
sorozco <sergiorozco.ochoa@gmail.com>
2024-11-18 20:22:12 -0500

    courseInfo 1.2 - Refactoring Content Component to avoid using multiple props

--- part1/courseInfo/src/App.jsx
+++ part1/courseInfo/src/App.jsx
@@ -1,5 +1,5 @@
 const Total = ({exercises1,exercises2,exercises3}) => {
-  return (<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>)
+  return (<p>Number of exercises: {exercises1 + exercises2 + exercises3}</p>)
 }
 
 const Header = ({course}) => {
@@ -8,37 +8,32 @@ const Header = ({course}) => {
   )
 }
 
-const Content = ({exercises1,exercises2,exercises3, part1, part2, part3}) => {
-  return (
-    <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-    </div>
-  )
-}
-
+const Content = ({Arr}) => (
+  <div>
+    {Arr.map(({part, exercises}) => (
+      <p key={part}>{part} - {exercises}</p>
+    ))}
+  </div>
+)
 
 
 const App = () => {
+
+  const Arrparts = [
+    {part:'Fundamentals of React', exercises: 10},
+    {part:'Using props to pass data',exercises: 7},
+    {part:'State of a component',exercises: 14}
+  ]
+
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
   const exercises2 = 7
-  const part3 = 'State of a component'
   const exercises3 = 14
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Content Arr={Arrparts} />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )
